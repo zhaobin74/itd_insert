1fd6ee63df3a5d93dc3b605b2a07999cfa6005e9
bzhao <Bin.Zhao@nasa.gov>
2020-12-14 20:14:25 -0500

    finished create_tile_rst function; it can write either saltwater_internal or seaicethermo_internal

--- write_ice_restart.py
+++ write_ice_restart.py
@@ -31,11 +31,13 @@ import functools
 
 MAPL_RHOWTR = 1000.0
 
-def create_tile_rst(fout, tilesize):
+from cice import (ncat, nilyr, nslyr)
 
-   icein = 'seaicethermo_internal_rst'
+def create_tile_rst(fin, fout, tilesize, aice_in, vice_in, 
+                 vsno_in, tskin_in, eice_in, esno_in, sst=None, sss=None):
 
-   with Dataset(icein) as src, Dataset(iceout, "w") as dst:
+
+   with Dataset(fin) as src, Dataset(fout, "w") as dst:
     # copy global attributes all at once via dictionary
     #dst.setncatts(src.__dict__)
     # copy dimensions
@@ -56,7 +58,7 @@ def create_tile_rst(fout, tilesize):
         dst[name].setncatts(src[name].__dict__)
 
     dst['time'][:] = 0  
-    dst['HSKINI'][:] = 0.5*1000.0 
+    dst['HSKINI'][:] = 0.5*MAPL_RHOWTR 
     dst['SSKINI'][:] = 30.0
     dst['QS'][:]     = 0.01
     dst['CH'][:]     = 1.0e-4
@@ -69,11 +71,47 @@ def create_tile_rst(fout, tilesize):
     dst['HPONDN'][:] = 0.0
     dst['TAUAGE'][:] = 0.0
     dst['VOLPOND'][:] = 0.0
+    if 'TWMTS' in src.variables:
+        dst['TWMTS'][:] = 0.0
+    if 'HSKINW' in src.variables:
+        dst['HSKINW'][:] =  2.0*MAPL_RHOWTR
+    dims_in = aice_in.shape
+    fr = dst['FR'][:]  
+    dims_ou = fr.shape
+    if dims_in[0] == dims_ou[0]:   
+        dst['FR'][:] = aice_in
+    else:
+        aiceout = np.zeros(fr.shape)
+        aiceout[1:,:] = aice_in
+        aiceout[0,:]  = np.maximum(0.0, np.minimum(1.0, 1.0-np.sum(aice_in,axis=0))) 
+        dst['FR'][:] = aiceout
+    dst['VOLICE'][:] = vice_in
+    dst['VOLSNO'][:] = vsno_in
+    dst['TSKINI'][:] = tskin_in
+    dst['ERGICE'][:] = eice_in
+    dst['ERGSNO'][:] = esno_in
+    if 'TSKINW' in src.variables:
+        if sst is None:
+            print 'missing SST data; required to populate TW field'
+            sys.exit(1) 
+        else:
+            dst['TSKINW'][:] = sst
+    if 'SSKINW' in src.variables:
+        if sss is None:
+            print 'missing SSS data; required to populate SW field'
+            sys.exit(1) 
+        else:
+            dst['SSKINW'][:] = sss
  
                
+if __name__ == "__main__":
 
-iceout = 'seaicethermo_internal_rst.copy'
-fout = 'seaicethermo_internal_rst.empty'
+   iceout = 'seaicethermo_internal_rst.copy'
+   saltout = 'saltwater_internal_rst.copy'
+   fout = 'seaicethermo_internal_rst.empty'
+   icein = 'seaicethermo_internal_rst'
+   saltin = 'saltwater_internal_rst'
 
-create_tile_rst(iceout, 24034000)
+   create_tile_rst(icein, iceout, 24034000)
+   create_tile_rst(saltin, saltout, 24034000)
 
